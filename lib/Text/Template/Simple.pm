5ac15b9015e9389386a030f945a07aedb23f1133
Burak Gürsoy <burak@cpan.org>
2009-04-10 08:49:34 +0300

    Test "resources" meta field

--- lib/Text/Template/Simple.pm
+++ lib/Text/Template/Simple.pm
@@ -2,7 +2,7 @@ package Text::Template::Simple;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '0.62_12';
+$VERSION = '0.62_13';
 
 use File::Spec;
 use Text::Template::Simple::Constants qw(:all);
