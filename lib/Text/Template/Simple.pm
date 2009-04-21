d55d3c8ebef0410220388acbc29dd2ccf3fe679b
Burak Gürsoy <burak@cpan.org>
2009-04-21 22:52:47 +0300

    update for new version

--- lib/Text/Template/Simple.pm
+++ lib/Text/Template/Simple.pm
@@ -2,7 +2,7 @@ package Text::Template::Simple;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '0.62_14';
+$VERSION = '0.62_15';
 
 use File::Spec;
 use Text::Template::Simple::Constants qw(:all);
