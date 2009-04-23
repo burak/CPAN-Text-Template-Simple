816fa2fef5399ab16c9a34b1c4ac4a3dd6d0e7f1
Burak Gürsoy <burak@cpan.org>
2009-04-23 19:53:08 +0300

    update builder

--- lib/Text/Template/Simple.pm
+++ lib/Text/Template/Simple.pm
@@ -2,7 +2,7 @@ package Text::Template::Simple;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '0.62_15';
+$VERSION = '0.62_16';
 
 use File::Spec;
 use Text::Template::Simple::Constants qw(:all);
