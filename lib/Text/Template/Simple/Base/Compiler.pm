69086be8a3524c91d0b2aa03a88bd760aca6edea
Burak Gürsoy <burak@cpan.org>
2009-09-21 20:08:20 +0300

    refactor to reduce complexity

--- lib/Text/Template/Simple/Base/Compiler.pm
+++ lib/Text/Template/Simple/Base/Compiler.pm
@@ -7,13 +7,10 @@ use Text::Template::Simple::Constants qw(:all);
 
 $VERSION = '0.80';
 
-sub _compile {
-   my $self  = shift;
-   my $tmpx  = shift || fatal('tts.base.compiler._compile.notmp');
-   my $param = shift || [];
-   my $opt   = shift || {};
+sub _init_compile_opts {
+   my $self = shift;
+   my $opt  = shift || {};
 
-   fatal('tts.base.compiler._compile.param') if not isaref($param);
    fatal('tts.base.compiler._compile.opt')   if not ishref($opt  );
 
    # set defaults
@@ -22,6 +19,52 @@ sub _compile {
    $opt->{chkmt}    ||= 0;  # check mtime of file template?
    $opt->{_sub_inc} ||= 0;  # are we called from a dynamic include op?
    $opt->{_filter}  ||= EMPTY_STRING; # any filters?
+
+   # first element is the shared names. if it's not defined, then there
+   # are no shared variables from top level
+   delete $opt->{_share}
+      if isaref($opt->{_share}) && ! defined $opt->{_share}[0];
+
+   $opt->{as_is} = $opt->{_sub_inc} && $opt->{_sub_inc} == T_STATIC;
+
+   return $opt;
+}
+
+sub _validate_chkmt {
+   my($self, $chkmt_ref, $tmpx) = @_;
+   ${chkmt_ref} = $self->[TYPE] eq 'FILE' ? (stat $tmpx)[STAT_MTIME]
+                    : do {
+                        DEBUG && LOG(DISABLE_MT =>
+                                     'Disabling chkmt. Template is not a file');
+                        0;
+                     };
+   return;
+}
+
+sub _compile_cache {
+   my($self, $tmp, $opt, $id_ref, $code_ref) = @_;
+   my $method = $opt->{id};
+   my @args   = (! $method || $method eq 'AUTO') ? ( $tmp              )
+              :                                    ( $method, 'custom' )
+              ;
+   ${$id_ref}  = $self->connector('Cache::ID')->new->generate( @args );
+
+   # prevent overwriting the compiled version in cache
+   # since we need the non-compiled version
+   ${$id_ref} .= '_1' if $opt->{as_is};
+
+   ${$code_ref} = $self->cache->hit( ${$id_ref}, $opt->{chkmt} );
+   LOG( CACHE_HIT =>  ${$id_ref} ) if DEBUG && ${$code_ref};
+   return;
+}
+
+sub _compile {
+   my $self  = shift;
+   my $tmpx  = shift || fatal('tts.base.compiler._compile.notmp');
+   my $param = shift || [];
+   my $opt   = $self->_init_compile_opts( shift );
+
+   fatal('tts.base.compiler._compile.param') if not isaref($param);
 
    my $tmp = $self->_examine( $tmpx );
    return $tmp if $self->[TYPE] eq 'ERROR';
@@ -37,42 +80,15 @@ sub _compile {
       $self->[NEEDS_OBJECT]++; # interpolated includes will need that
    }
 
-   if ( $opt->{chkmt} ) {
-      $opt->{chkmt} = $self->[TYPE] eq 'FILE' ? (stat $tmpx)[STAT_MTIME]
-                    : do {
-                        DEBUG && LOG(DISABLE_MT =>
-                                     'Disabling chkmt. Template is not a file');
-                        0;
-                     }
-   }
+   $self->_validate_chkmt( \$opt->{chkmt}, $tmpx ) if $opt->{chkmt};
 
    LOG( COMPILE => $opt->{id} ) if DEBUG && defined $opt->{id};
 
-   my($CODE, $ok);
    my $cache_id = EMPTY_STRING;
 
-   my $as_is = $opt->{_sub_inc} && $opt->{_sub_inc} == T_STATIC;
-
-   # first element is the shared names. if it's not defined, then there
-   # are no shared variables from top level
-   delete $opt->{_share}
-      if isaref($opt->{_share}) && ! defined $opt->{_share}[0];
-
+   my($CODE);
    if ( $self->[CACHE] ) {
-      my $method = $opt->{id};
-      my @args   = (! $method || $method eq 'AUTO') ? ( $tmp              )
-                 :                                    ( $method, 'custom' )
-                 ;
-      $cache_id  = $self->connector('Cache::ID')->new->generate( @args );
-
-      # prevent overwriting the compiled version in cache
-      # since we need the non-compiled version
-      $cache_id .= '_1' if $as_is;
-
-      if ( $CODE = $self->cache->hit( $cache_id, $opt->{chkmt} ) ) {
-         LOG( CACHE_HIT =>  $cache_id ) if DEBUG;
-         $ok = 1;
-      }
+      $self->_compile_cache( $tmp, $opt, \$cache_id, \$CODE );
    }
 
    $self->cache->id( $cache_id ); # if $cache_id;
@@ -84,23 +100,7 @@ sub _compile {
       SHARED_VARS => "Adding shared variables ($shead) from a dynamic include"
    ) if DEBUG && $shead;
 
-   if ( not $ok ) {
-      # we have a cache miss; parse and compile
-      LOG( CACHE_MISS => $cache_id ) if DEBUG;
-
-      my $restore_header;
-      if ( $shead ) {
-         my $param_x = join q{,}, ('shift') x @sparam;
-         my $shared  = sprintf q~my(%s) = (%s);~, $shead, $param_x;
-         $restore_header = $self->[HEADER];
-         $self->[HEADER] = $shared . q{;} . ( $self->[HEADER] || EMPTY_STRING );
-      }
-
-      my %popt   = ( %{ $opt }, cache_id => $cache_id, as_is => $as_is );
-      my $parsed = $self->_parse( $tmp, \%popt );
-      $CODE      = $self->cache->populate( $cache_id, $parsed, $opt->{chkmt} );
-      $self->[HEADER] = $restore_header if $shead;
-   }
+   $CODE = $self->_cache_miss( $cache_id, $shead, \@sparam, $opt, $tmp ) if ! $CODE;
 
    my @args;
    push @args, $self   if $self->[NEEDS_OBJECT]; # must be the first
@@ -113,6 +113,26 @@ sub _compile {
       if $opt->{_filter};
 
    return $out;
+}
+
+sub _cache_miss {
+   my($self, $cache_id, $shead, $sparam, $opt, $tmp) = @_;
+   # we have a cache miss; parse and compile
+   LOG( CACHE_MISS => $cache_id ) if DEBUG;
+
+   my $restore_header;
+   if ( $shead ) {
+      my $param_x = join q{,}, ('shift') x @{ $sparam };
+      my $shared  = sprintf q~my(%s) = (%s);~, $shead, $param_x;
+      $restore_header = $self->[HEADER];
+      $self->[HEADER] = $shared . q{;} . ( $self->[HEADER] || EMPTY_STRING );
+   }
+
+   my %popt   = ( %{ $opt }, cache_id => $cache_id, as_is => $opt->{as_is} );
+   my $parsed = $self->_parse( $tmp, \%popt );
+   my $CODE      = $self->cache->populate( $cache_id, $parsed, $opt->{chkmt} );
+   $self->[HEADER] = $restore_header if $shead;
+   return $CODE;
 }
 
 sub _call_filters {
